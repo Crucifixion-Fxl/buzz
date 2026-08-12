dc2191d5acc7cd384bde88fd678d7aaace14472a
Taylor Ho <taylorkmho@gmail.com>
2026-08-12 00:23:43 -0700

    feat(mobile): render self-describing permalink chips
    Co-authored-by: Taylor Ho <taylorkmho@gmail.com>
    Signed-off-by: Taylor Ho <taylorkmho@gmail.com>

--- mobile/test/features/channels/channel_detail_page_test.dart
+++ mobile/test/features/channels/channel_detail_page_test.dart
@@ -3803,7 +3803,7 @@ void main() {
       await tester.pumpAndSettle();
       final initialPushCount = observer.pushCount;
 
-      await tester.tap(find.text('#random'));
+      await tester.tap(find.text('random'));
       await tester.pumpAndSettle();
 
       expect(observer.pushCount, initialPushCount + 1);
@@ -3835,7 +3835,7 @@ void main() {
       await tester.pumpAndSettle();
 
       channelsNotifier.setChannels([_testChannel]);
-      await tester.tap(find.text('#random'));
+      await tester.tap(find.text('random'));
       await tester.pump();
 
       expect(find.text('Channel could not be opened'), findsOneWidget);
@@ -3889,7 +3889,7 @@ void main() {
       await tester.pumpAndSettle();
       final initialPushCount = observer.pushCount;
 
-      await tester.tap(find.text('#random').last);
+      await tester.tap(find.text('random').last);
       await tester.pumpAndSettle();
 
       expect(observer.pushCount, initialPushCount + 1);
